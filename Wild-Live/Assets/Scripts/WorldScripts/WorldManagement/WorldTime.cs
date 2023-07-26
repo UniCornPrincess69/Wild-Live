f589734c5c8afa494d8e3424cce91d15e1134a57
UniCornPrincess69 <115454083+UniCornPrincess69@users.noreply.github.com>
2023-07-26 18:58:59 +0200

    Weathersystem, WorldTime, UI
    Created an empty script for the weather system, created a small script with the world time(sun movement), started to create UI to adjust game speed

---
+++ Wild-Live/Assets/Scripts/WorldScripts/WorldManagement/WorldTime.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldTime : MonoBehaviour
+{
+
+    #region Variables
+    [field: SerializeField, Range(1f, 20f)]
+    public float TimeSpeed { get; set; } = 0f;
+
+    private Transform _light = null;
+    private float _lightRot = 50f;
+    #endregion
+    void Start()
+    {
+        _light = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        _lightRot += TimeSpeed * Time.deltaTime;
+        SetRotation( _lightRot );
+    }
+
+    /// <summary>
+    /// Setting the rotation of the main light source (sun)
+    /// </summary>
+    /// <param name="xRot">Float for the x axis rotation</param>
+    public void SetRotation(float xRot) 
+    {
+        _light.rotation = Quaternion.Euler(xRot, _light.rotation.y, _light.rotation.z);
+    }
+}
