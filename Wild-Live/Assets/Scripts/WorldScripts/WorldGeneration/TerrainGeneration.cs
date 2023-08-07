95f7a62f2ed160908ba62072a60a19df7af4b7cb
UniCornPrincess69 <115454083+UniCornPrincess69@users.noreply.github.com>
2023-08-07 17:36:27 +0200

    Further work on the terrain generation
    Stephan helped me to get the terrain seamless

--- Wild-Live/Assets/Scripts/WorldScripts/WorldGeneration/TerrainGeneration.cs
+++ Wild-Live/Assets/Scripts/WorldScripts/WorldGeneration/TerrainGeneration.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.ConstrainedExecution;
+using System.Threading;
 using Unity.VisualScripting;
 using UnityEngine;
-
+using UnityEngine.Profiling;
 
 [RequireComponent(typeof(Terrain))]
 public class TerrainGeneration : MonoBehaviour
@@ -12,10 +13,11 @@ public class TerrainGeneration : MonoBehaviour
     private Vector2 _offset = Vector2.zero;
     [SerializeField] private Texture2D _texture = null;
     [SerializeField] private string _seed = string.Empty;
-    [SerializeField] private float _offsetCoord = 0f;
+    [SerializeField] private Vector2 _offsetCoord = Vector2.zero;
     [SerializeField] private float _octaveCount = 1f;
     [SerializeField] private float _scale = 1f;
     [SerializeField] private bool _useHeightmap = false;
+    [SerializeField, Range(0.1f, 1.0f)] private float _heightMapIntensity = 0.1f;
     private int _terrainWidth = 0;
     private int _terrainHeight = 0;
 
@@ -26,14 +28,19 @@ public class TerrainGeneration : MonoBehaviour
         _terrain = GetComponent<Terrain>();
         _terrainWidth = _terrain.terrainData.heightmapResolution;
         _terrainHeight = _terrain.terrainData.heightmapResolution;
+        
     }
 
-    void Start()
+    IEnumerator Start()
     {
+        yield return new WaitForSeconds(1);
         Random.InitState(_seed.GetHashCode());
         _offset = new Vector2(Random.Range(0, 100_000), Random.Range(0, 100_000));
         if (_useHeightmap) TerrainWithHeightmap();
-        else GenerateTerrain();
+        else
+        {
+            GenerateTerrain();
+        }
     }
 
     //If a heightmap is used this method is called. Inspired via ChatGPT
@@ -52,9 +59,11 @@ public class TerrainGeneration : MonoBehaviour
             }
         }
     }
-
+    //TODO: Before and after Thread screenshots
     public void GenerateTerrain()
     {
+        Profiler.BeginSample("Terrain Gen " + gameObject.name);
+
         float[,] heights = new float[_terrainWidth, _terrainHeight];
         for (int y = 0; y < _terrainWidth; y++)
         {
@@ -62,11 +71,13 @@ public class TerrainGeneration : MonoBehaviour
             {
                 var noiseVal = GetHeightVal(x, y);
                 var heightMapVal = 0f;
-                if(_useHeightmap) heightMapVal = GetHeighmapVal(x, y);
+                if (_useHeightmap) heightMapVal = GetHeighmapVal(x, y);
                 heights[x, y] = noiseVal + heightMapVal;
             }
         }
+
         _terrain.terrainData.SetHeights(0, 0, heights);
+        Profiler.EndSample();
     }
 
     private float GetHeighmapVal(float x, float y)
@@ -75,15 +86,20 @@ public class TerrainGeneration : MonoBehaviour
 
         Color color = _texture.GetPixel((int)x, (int)y);
 
-        result = color.grayscale * 0.1f;
+        result = color.grayscale * _heightMapIntensity;
 
         return result;
     }
 
     private float GetHeightVal(float x, float y)
     {
-        var xCoord = x / _terrainWidth * _scale + _offset.x;
-        var yCoord = y / _terrainHeight * _scale + _offset.y;
+        // calculating the Pixel index of the PerlinNoise texture using the World position of the Vertex
+        var xCoord = x / _terrainWidth * 0.5f + _offsetCoord.y;
+        var yCoord = y / _terrainHeight * 0.5f + _offsetCoord.x;
+
+        // adding a offset for the PerlinNoisePixel if the terrains are moved
+        xCoord -= (_offsetCoord.y > 0 ? 1f / 2050 : 0);
+        yCoord -= (_offsetCoord.x > 0 ? 1f / 2050 : 0);
 
 
         float scale = _scale;
@@ -93,7 +109,7 @@ public class TerrainGeneration : MonoBehaviour
 
         for (int i = 0; i < _octaveCount; i++)
         {
-            noiseVal += Mathf.PerlinNoise(xCoord * scale + _offsetCoord, yCoord * scale + _offsetCoord) * intensity;
+            noiseVal += Mathf.PerlinNoise(xCoord * scale + _offset.x, yCoord * scale + _offset.y) * intensity;
             maxHeight += intensity;
 
             scale *= 2;
